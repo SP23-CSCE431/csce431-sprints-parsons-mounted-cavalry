735550d89864e401f7b416f29565d64c8a700d35
aruzedo <aruzedo@tamu.edu>
2023-02-13 18:19:10 -0600

    changes to horse unit test

--- spec/unit/unit_horse_spec.rb
+++ spec/unit/unit_horse_spec.rb
@@ -1,26 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Horse, type: :model do
-    before(:all) do
-        @horse = Horse.create(name: "Horse", brand: 'H1199', birthday: '01/01/1999')
+    subject do
+        described_class.new(name: "Horse", brand: 'H1199', birthday: '01/01/1999')
     end
 
   it 'is valid with valid attributes' do
-    expect(@horse).to be_valid
+    expect(subject).to be_valid
   end
 
   it 'is valid that horse can be updated' do
-    @horse.update(:name => "Horse1")
-    expect(Horse.find_by_name("Horse1")).to eq(@horse)
+    subject.update(:name => "Horse1")
+    subject.update(:brand => "H1198")
+    subject.update(:birthday => "01/02/1999")
+    expect(Horse.find_by_name("Horse1")).to eq(subject)
+    expect(Horse.find_by_brand("H1198")).to eq(subject)
+    expect(Horse.find_by_birthday("01/02/1999")).to eq(subject)
   end
 
-  it 'is that a horse can be destroyed' do
-    @horse.destroy
+  it 'is valid that horse can be deleted' do
+    subject.destroy
     expect(Horse.find_by_name("Horse1")).to be_nil
   end
-
+  
   it 'is not valid without a name' do
     subject.name = nil
     expect(subject).not_to be_valid
   end
+  
+  it 'is not valid without a brand' do
+    subject.brand = nil
+    expect(subject).not_to be_valid
+  end
+  
+  it 'is not valid without a birthday' do
+    subject.birthday = nil
+    expect(subject).not_to be_valid
+  end
+
 end
