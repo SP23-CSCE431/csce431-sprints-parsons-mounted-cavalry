ba833772d60d137eba95b97f5b9acface9769136
aruzedo <aruzedo@tamu.edu>
2023-02-13 18:57:36 -0600

    fix for cadet delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_02_13_122400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_122400) do
     t.string "phone_number"
     t.string "uin"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "horses", force: :cascade do |t|
