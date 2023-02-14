dbcb195199cbabbbb2b1380c19c3b68554c239a4
kiara-bermudez <kiarabermudez.10@gmail.com>
2023-02-13 18:41:56 -0600

    add edit and delete unit test. pass cadet unit tests

--- spec/unit/unit_cadet_spec.rb
+++ spec/unit/unit_cadet_spec.rb
@@ -9,6 +9,24 @@ RSpec.describe Cadet, type: :model do
     expect(subject).to be_valid
   end
 
+  it 'is valid that cadet can be updated' do
+    subject.update(:first_name => "Billy2")
+    subject.update(:last_name => "Bob2")
+    subject.update(:graduation_year => "2025")
+    subject.update(:phone_number => "9876543210")
+    subject.update(:uin => "654300021")
+    expect(Cadet.find_by_first_name("Billy2")).to eq(subject)
+    expect(Cadet.find_by_last_name("Bob2")).to eq(subject)
+    expect(Cadet.find_by_graduation_year("2025")).to eq(subject)
+    expect(Cadet.find_by_phone_number("9876543210")).to eq(subject)
+    expect(Cadet.find_by_uin("654300021")).to eq(subject)
+  end
+
+  it 'is valid that cadet can be deleted' do
+    subject.destroy
+    expect(Cadet.find_by_first_name("Billy2")).to be_nil
+  end
+
   it 'is not valid without a first name' do
     subject.first_name = nil
     expect(subject).not_to be_valid
