a63e2b26cc761508332208639ab16f0c1f652949
ZThygesen <zsthygesen@gmail.com>
2023-03-02 23:51:29 -0600

    write unit tests for attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,6 @@
 class Attendance < ApplicationRecord
-    validates :schedule_id, :date, :purpose, presence: true
+    validates :schedule_id, :date, presence: true
+    validates :purpose, inclusion: { in: ['Ride', 'Work', 'Ride & Work', 'Training', 'Sections', 'No']}, presence: true
 
     belongs_to :schedule
     belongs_to :horse
--- spec/unit/unit_attendance_spec.rb
+++ spec/unit/unit_attendance_spec.rb
@@ -1,80 +1,179 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-    subject do
-        described_class.new(attendance_id: 1, cadet_id: 1, horse_id: 1, staff_id: 1, appointment_time: '01/01/2023', check_in_time: '01/01/2023', check_out_time: '01/01/2023', purpose: "Riding Lesson")
+  # create instances of reference that are used in creation of attendances 
+  let(:user) { User.create(is_admin: true, is_staff: true, first_name: 'John', last_name: 'Doe', classification: 'Senior', skill_level: 'Advanced', phone_number: '123-456-7890', email: 'j.doe@tamu.edu') }
+  let(:schedule) { Schedule.create(user_id: user.id, recurrence: 'MWF') }
+  let(:horse) { Horse.create(name: 'Ed', brand: 'B12', herd: 'Charlie', difficulty: 'Intermediate', condition: 'Healthy') }
+
+  # sunny day case
+  subject do
+    described_class.new(schedule_id: schedule.id, horse_id: horse.id, date: '2023-12-25', check_in_time: nil, purpose: 'Training')
+  end
+
+  # validate creation of attendances
+  describe 'create' do
+    describe 'schedule selection' do
+      it 'has valid schedule' do
+        expect(subject).to be_valid
+      end
+      it 'has no schedule' do
+        attendance = Attendance.new(horse_id: horse.id, date: '2023-12-25', check_in_time: nil, purpose: 'Training')
+        expect(attendance).not_to be_valid
+      end
+      it 'has nil schedule' do 
+        attendance = Attendance.new(schedule_id: nil, horse_id: horse.id, date: '2023-12-25', check_in_time: nil, purpose: 'Training')
+        expect(attendance).not_to be_valid
+      end
+      it 'has invalid/non-existent schedule' do
+        attendance = Attendance.new(schedule_id: -1, horse_id: horse.id, date: '2023-12-25', check_in_time: nil, purpose: 'Training')
+        expect(attendance).not_to be_valid
+      end
     end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+    describe 'horse selection' do
+      it 'has valid horse' do
+        expect(subject).to be_valid
+      end
+      it 'has no horse is okay' do
+        attendance = Attendance.new(schedule_id: schedule.id, date: '2023-12-25', check_in_time: nil, purpose: 'Training')
+        expect(attendance).to be_valid
+      end
+      it 'has nil horse is okay' do 
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: nil, date: '2023-12-25', check_in_time: nil, purpose: 'Training')
+        expect(attendance).to be_valid
+      end
+      it 'has invalid/non-existent horse' do
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: -1, date: '2023-12-25', check_in_time: nil, purpose: 'Training')
+        expect(attendance).not_to be_valid
+      end
+    end
+    describe 'date selection' do
+      it 'has valid date' do
+        expect(subject).to be_valid
+      end
+      it 'has invalid day' do
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: horse.id, date: '2023-12-32', check_in_time: nil, purpose: 'Training')
+        expect(attendance).not_to be_valid
+      end
+      it 'has invalid month' do
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: horse.id, date: '2023-13-25', check_in_time: nil, purpose: 'Training')
+        expect(attendance).not_to be_valid
+      end
+      it 'has nil date' do
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: horse.id, date: nil, check_in_time: nil, purpose: 'Training')
+        expect(attendance).not_to be_valid
+      end
+      it 'has empty date' do
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: horse.id, date: '', check_in_time: nil, purpose: 'Training')
+        expect(attendance).not_to be_valid
+      end
+    end
+    describe 'check in time selection' do
+      it 'is not initially instantiated' do
+        expect(subject).to be_valid
+      end
+    end
+    describe 'purpose selection' do
+      it 'has valid purpose' do
+        expect(subject).to be_valid
+      end
+      it 'has invalid purpose' do
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: horse.id, date: '2023-12-25', check_in_time: nil, purpose: 'Running')
+        expect(attendance).not_to be_valid
+      end
+      it 'has nil purpose' do
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: horse.id, date: '2023-12-25', check_in_time: nil, purpose: nil)
+        expect(attendance).not_to be_valid
+      end
+      it 'has empty purpose' do
+        attendance = Attendance.new(schedule_id: schedule.id, horse_id: horse.id, date: '2023-12-25', check_in_time: nil, purpose: '')
+        expect(attendance).not_to be_valid
+      end
+    end
   end
 
-  it 'is valid that attendance can be updated' do
-    subject.update(:attendance_id => 2)
-    subject.update(:cadet_id => 2)
-    subject.update(:horse_id => 2)
-    subject.update(:staff_id => 2)
-    subject.update(:appointment_time => '01/02/2023')
-    subject.update(:check_in_time => '01/02/2023')
-    subject.update(:check_out_time => '01/02/2023')
-    subject.update(:purpose => "Feeding")
-    expect(Attendance.find_by_attendance_id(2)).to eq(subject)
-    expect(Attendance.find_by_cadet_id(2)).to eq(subject)
-    expect(Attendance.find_by_horse_id(2)).to eq(subject)
-    expect(Attendance.find_by_staff_id(2)).to eq(subject)
-    expect(Attendance.find_by_appointment_time('01/02/2023')).to eq(subject)
-    expect(Attendance.find_by_check_in_time('01/02/2023')).to eq(subject)
-    expect(Attendance.find_by_check_out_time('01/02/2023')).to eq(subject)
-    expect(Attendance.find_by_purpose("Feeding")).to eq(subject)
+  # validate updating of attendances
+  describe 'update' do
+    describe 'schedule change' do
+      it 'valid schedule change' do
+        new_user = User.create(is_admin: true, is_staff: true, first_name: 'John', last_name: 'Doe', classification: 'Senior', skill_level: 'Advanced', phone_number: '123-456-7890', email: 'j.doe@tamu.edu')
+        new_schedule = Schedule.create(user_id: new_user.id, recurrence: 'MWF')
+        subject.update(:schedule_id => new_schedule.id)
+        expect(Attendance.find_by_schedule_id(new_schedule.id)).to eq(subject)
+      end
+      it 'update with nil schedule' do
+        subject.update(:schedule_id => nil)
+        expect(subject).not_to be_valid
+      end
+      it 'update with invalid/non-existent schedule' do
+        subject.update(:schedule_id => -1)
+        expect(subject).not_to be_valid
+      end
+    end
+    describe 'horse change' do
+      it 'valid horse change' do
+        new_horse = Horse.create(name: 'Ed', brand: 'B12', herd: 'Charlie', difficulty: 'Intermediate', condition: 'Healthy')
+        subject.update(:horse_id => new_horse.id)
+        expect(Attendance.find_by_horse_id(new_horse.id)).to eq(subject)
+      end
+      it 'update with nil horse' do
+        subject.update(:horse_id => nil)
+        expect(subject).not_to be_valid
+      end
+      it 'update with invalid/non-existent horse' do
+        subject.update(:horse_id => -1)
+        expect(subject).not_to be_valid
+      end
+    end
+    describe 'date change' do
+      it 'valid date change' do
+        subject.update(:date => '2024-07-03')
+        expect(Attendance.find_by_date('2024-07-03')).to eq(subject)
+      end
+      it 'update with empty date' do
+        subject.update(:date => '')
+        expect(subject).not_to be_valid
+      end
+      it 'update with nil date' do
+        subject.update(:date => nil)
+        expect(subject).not_to be_valid
+      end
+      it 'update with invalid day' do
+        subject.update(:date => '2023-12-32')
+        expect(subject).not_to be_valid
+      end
+      it 'update with invalid month' do
+        subject.update(:date => '2023-13-25')
+        expect(subject).not_to be_valid
+      end
+    end
+    describe 'purpose change' do
+      it 'valid purpose change' do
+        subject.update(:purpose => 'Work')
+        expect(Attendance.find_by_purpose('Work')).to eq(subject)
+      end
+      it 'update with empty purpose' do
+        subject.update(:purpose => '')
+        expect(subject).not_to be_valid
+      end
+      it 'update with nil purpose' do
+        subject.update(:purpose => nil)
+        expect(subject).not_to be_valid
+      end
+      it 'update with invalid purpose' do
+        subject.update(:purpose => 'Running')
+        expect(subject).not_to be_valid
+      end
+    end
   end
 
-  it 'is valid that attendance can be destroyed' do
-    subject.destroy
-    expect(Attendance.find_by_attendance_id(2)).to be_nil
-  end
-  
-  # it 'is not valid without a attendance_id' do
-  #   subject.attendance_id = nil
-  #   expect(subject).not_to be_valid
-  # end
-  
-  # it 'is not valid without a cadet_id' do
-  #   subject.cadet_id = nil
-  #   expect(subject).not_to be_valid
-  # end
-  
-  # it 'is not valid without a horse_id' do
-  #   subject.horse_id = nil
-  #   expect(subject).not_to be_valid
-  # end
-  
-  # it 'is not valid without a staff_id' do
-  #   subject.staff_id = nil
-  #   expect(subject).not_to be_valid
-  # end
-  
-  it 'is not valid without a appointment_time' do
-    subject.appointment_time = nil
-    expect(subject).not_to be_valid
-  end
-
-  it 'is not valid without an appointment_time' do
-    subject.appointment_time = nil
-    expect(subject).not_to be_valid
-  end
-    
-  it 'is not valid without a check_in_time' do
-    subject.check_in_time = nil
-    expect(subject).not_to be_valid
-  end
-    
-  it 'is not valid without a check_out_time' do
-    subject.check_out_time = nil
-    expect(subject).not_to be_valid
-  end
-    
-  it 'is not valid without a purpose' do
-    subject.purpose = nil
-    expect(subject).not_to be_valid
+  # validate deletion of attendances
+  describe 'delete' do
+    it 'attendance can be deleted' do
+      expect(subject).to be_valid
+      subject_id = subject.id
+      subject.destroy
+      expect(Attendance.find_by_id(subject_id)).to be_nil
+    end
   end
 end
