9fe2a418f26ccc3478578f3c188758245acda693
ZThygesen <zsthygesen@gmail.com>
2023-03-03 20:41:11 -0600

    populate seed file and remove outdated test files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
-Staff.create!([
-  {is_admin: false, first_name: "Mackenzie", last_name: "Tran", phone_number: "1", email: "mt@gmail.com", password: "1"}
+User.create!([
+  {is_admin: true, is_staff: false, first_name: "John", last_name: "Doe", classification: "Super Senior", skill_level: "Advanced", phone_number: "123-456-7890", email: "john.doe@tamu.edu"},
+  {is_admin: false, is_staff: true, first_name: "Jane", last_name: "Doe", classification: "Senior", skill_level: "Intermediate", phone_number: "111-222-3333", email: "jane.doe@tamu.edu"},
+  {is_admin: false, is_staff: false, first_name: "Billy", last_name: "Bob", classification: "Sophomore", skill_level: "Beginner", phone_number: "435-675-4345", email: "billy.bob@tamu.edu"}
 ])
 Horse.create!([
+  {name: "Horsey", brand: "H1149", herd: "Bravo", difficulty: "Intermediate", condition: "Healthy"},
+  {name: "Ed", brand: "J9822", herd: "Delta", difficulty: "Advanced", condition: "Healthy"},
+  {name: "Speedy", brand: "S711", herd: "Bravo", difficulty: "Easy", condition: "Healthy"}
+])
   
