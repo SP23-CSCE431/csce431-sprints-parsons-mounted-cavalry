96314b1958b308d024ae132b03affd53e336e943
aruzedo <aruzedo@tamu.edu>
2023-03-06 16:05:33 -0600

    added coverage to gitignore

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  #creates a mock token for the integration tests
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
       :provider => "google_oauth2",
