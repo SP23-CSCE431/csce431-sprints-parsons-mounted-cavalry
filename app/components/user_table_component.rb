958561effc30d7b0ba9b1ac468326fea90f8261c
Martin Wnoroski <martin.wnorowski@tamu.edu>
2023-03-28 22:47:41 -0500

    update UI to match Figma

--- app/components/user_table_component.rb
+++ app/components/user_table_component.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
 
 class UserTableComponent < ViewComponent::Base
-  def initialize(user)
-    super
-    @user = user
+  def initialize(cadets, staffs)
+      super @cadets = cadets
+      super @staffs = staffs
   end
+
+  # maps the given user's skill level to a color, returns the color
+  def get_user_skill_color(user)
+    colors = { 'Beginner' => '#A7F1A8', 'Intermediate' => '#FFF36D', 'Advanced' => '#F47174' }
+
+    if user.nil?
+        'none'
+    else
+        colors[user.skill_level]
+    end
+  end
+
+  # maps the given user's permission level to a color, returns the color
+  def get_user_permission_color(user)
+    colors = { 'Admin' => '#A7F1A8', 'Staff' => '#F47174'}
+  
+    if user.nil?
+      'none'
+    else
+      if user.is_admin
+        colors['Admin']
+      elsif user.is_staff
+        colors['Staff']
+      else
+        'none'
+      end
+    end
+  end  
 end
