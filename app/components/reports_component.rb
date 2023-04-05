1c61e7da020f6e16a40846b3edd487d5e729ff81
wnorowski <44731957+wnorowski@users.noreply.github.com>
2023-04-05 01:48:00 -0500

    update Reports page (#31)
    * update logo
    
    * fix frontpage logo
    
    * update reports page

--- app/components/reports_component.rb
+++ app/components/reports_component.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class ReportsComponent < ViewComponent::Base
-    def initialize(horses, cadets, staffs)
+    def initialize(horses, cadets, staffs, start_day, end_day, curr_day)
         super @horses = horses
         super @cadets = cadets
         super @staffs = staffs
+        super @start_day = start_day
+        super @end_day = end_day
+        super @curr_day = curr_day
     end
 end
